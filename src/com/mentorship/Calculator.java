fc920bf873c42c8430c8d6b40db55e2152d81558
olesmack <oles.mack@gmail.com>
2022-03-25 16:23:56 +0200

    Add file Calculator.java

---
+++ src/com/mentorship/Calculator.java
@@ -0,0 +1,20 @@
+package com.mentorship;
+
+public class Calculator {
+    float a;
+    float b;
+    char operation;
+
+    public float add() {
+        return a + b;
+    }
+    public float sub() {
+        return a - b;
+    }
+    public float mul() {
+        return a * b;
+    }
+    public float div() {
+        return a / b;
+    }
+}
