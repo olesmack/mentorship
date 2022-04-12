f520ace71957d1c0614529a4689dbf5a4f95e024
olesmack <oles.mack@gmail.com>
2022-04-12 19:49:41 +0300

    Removed unused imports

--- src/test/java/pageobjects/CategoriesPage.java
+++ src/test/java/pageobjects/CategoriesPage.java
@@ -2,7 +2,6 @@ package pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CategoriesPage {
 
--- src/test/java/pageobjects/ProductPage.java
+++ src/test/java/pageobjects/ProductPage.java
@@ -2,8 +2,6 @@ package pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
 
 public class ProductPage {
 
